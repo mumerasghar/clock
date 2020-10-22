73ffdf65cdaa62c3c42fd3ad785d776f0b292066
umer <mumer>
2020-10-23 02:20:54 +0500

    CLock

--- src/index.js
+++ src/index.js
@@ -1,17 +1,82 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Child extends React.Component {
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+        }
+    }
+
+    componentDidMount() {
+        this.timerId = setInterval(() => this.tick(), 100)
+    }
+
+    componentWillUnmount() {
+        console.log("In unmount")
+        clearInterval(this.timerId);
+    }
+
+    tick() {
+        console.log("In Timer")
+        this.setState({
+            date: new Date()
+        })
+    }
+
+    render() {
+        return (
+            <div onClick={() => this.props.clickHandle()}>
+                {this.state.date.toLocaleTimeString()}
+            </div>
+        );
+    }
+}
+
+class Clock extends React.Component {
+
+    constructor(props) {
+        super(props);
+        console.log("Constructor")
+        this.state = {
+            show: true
+        }
+    }
+
+    clickHandle() {
+        console.log("Click Handle")
+        this.setState({
+            show: false
+        })
+    }
+
+    render() {
+        let child;
+        if (this.state.show) {
+            child = <Child clickHandle={() => this.clickHandle()}/>
+        }
+
+        return (
+            <div>
+                {child}
+            </div>
+        )
+    }
+}
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <Clock specific="1"/>
+                <Clock specific="2"/>
+                <Clock specific="3"/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(<App/>, document.getElementById('root'))
